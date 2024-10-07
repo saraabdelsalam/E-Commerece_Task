958e48bac5c0c141840c818e1f7d45323058b231
sara abdelsalam <105568705+saraabdelsalam@users.noreply.github.com>
2024-10-07 19:43:23 +0300

    login endpoint & admin only endpoint implementation

--- Api/UserController.py
+++ Api/UserController.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, jsonify
 from Services.UserService import register_user
 from Services.AuthService import login_user
-from flask_jwt_extended import jwt_required, get_jwt_identity
+from flask_jwt_extended import jwt_required, get_jwt_identity, get_current_user
 
 bp = Blueprint('api', __name__)
 
@@ -9,28 +9,27 @@ bp = Blueprint('api', __name__)
 def register():
     try:
         user_data = request.json
-        # Pass user data (including role) to the registration service
         register_user(user_data)
         return jsonify({"message": "User registered successfully."}), 201
     except Exception as e:
         return jsonify({"message": str(e)}), 400
 
 
-# @bp.route('/login', methods=['POST'])
-# def login():
-#     email = request.json.get('email')
-#     password = request.json.get('password')
+@bp.route('/login', methods=['POST'])
+def login():
+    email = request.json.get('email')
+    password = request.json.get('password')
     
-#     try:
-#         token = login_user(email, password)
-#         return jsonify(access_token=token), 200
-#     except Exception as e:
-#         return jsonify({"message": str(e)}), 401
+    try:
+        token = login_user(email, password)
+        return jsonify(access_token=token), 200
+    except Exception as e:
+        return jsonify({"message": str(e)}), 401
 
-# @bp.route('/admin_only_page', methods=['GET'])
-# @jwt_required()
-# def admin_only_page():
-#     current_user = get_jwt_identity()
-#     if current_user['role'] != 2:  # Check if user is admin
-#         return jsonify({"message": "Access forbidden: Admins only."}), 403
-#     return jsonify({"message": f"Hello {current_user['email']}, welcome to the admin page!"})
+@bp.route('/c', methods=['GET'])
+@jwt_required()
+def admin_only_page():
+    current_user = get_jwt_identity()
+    if current_user['role'] != 2: 
+        return jsonify({"message": "Access forbidden: Admins only."}), 403
+    return jsonify({"message": f"Hello {current_user['first_name']}, welcome to the admin page!"})
--- Services/UserService.py
+++ Services/UserService.py
@@ -7,21 +7,17 @@ def register_user(user_data):
     if get_user_by_email(user_data['email']):
         raise Exception("User already exists")
 
-    # Get the role from the request, default to 'user' if not provided
     userRole = UserRole.get_user_role(user_data)
     
-
-    # Hash the password
     user_data['password'] = generate_password_hash(user_data['password'])
     
-    # Create the new User with the integer value of the role enum
     new_user = User(
         first_name=user_data['first_name'],
         last_name=user_data['last_name'],
         email=user_data['email'],
         phone_number=user_data['phone_number'],
         password=user_data['password'],
-        role=userRole # Store the integer value of the Enum in the database
+        role=userRole
     )
     
     add_user(new_user)
